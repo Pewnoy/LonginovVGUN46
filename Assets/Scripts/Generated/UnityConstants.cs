d992984a1c25cd0830e82279ef0282ed4b6f73c6
Pew <memeris2@gmail.com>
2026-07-15 10:19:43 +0300

    Работа над UI
    А если точнее, работа над CarnageIndicator, Speedometer, и анимация перезарядки на танке, также был изменен уровень с тестом скорости.

--- Assets/Scripts/Generated/UnityConstants.cs
+++ Assets/Scripts/Generated/UnityConstants.cs
@@ -13,34 +13,22 @@ namespace Tanks
 			IgnoreRaycast = 1 << 2,
 			Water = 1 << 4,
 			UI = 1 << 5,
-			Projectiles = 1 << 6,
-			Obstacles = 1 << 7,
-			Ground = 1 << 8,
-			Players = 1 << 9,
-			PostProcessing = 1 << 10,
-		}
+            Obstacles = 1 << 6,
+        }
 
 		public const int DefaultLayerInt = 0;
 		public const int TransparentFXLayerInt = 1;
 		public const int IgnoreRaycastLayerInt = 2;
 		public const int WaterLayerInt = 4;
 		public const int UILayerInt = 5;
-		public const int ProjectilesLayerInt = 6;
-		public const int ObstaclesLayerInt = 7;
-		public const int GroundLayerInt = 8;
-		public const int PlayersLayerInt = 9;
-		public const int PostProcessingLayerInt = 10;
 
 		public const string DefaultLayerName = "Default";
 		public const string TransparentFXLayerName = "TransparentFX";
 		public const string IgnoreRaycastLayerName = "IgnoreRaycast";
 		public const string WaterLayerName = "Water";
 		public const string UILayerName = "UI";
-		public const string ProjectilesLayerName = "Projectiles";
-		public const string ObstaclesLayerName = "Obstacles";
-		public const string GroundLayerName = "Ground";
-		public const string PlayersLayerName = "Players";
-		public const string PostProcessingLayerName = "PostProcessing";
+        public const int ObstaclesLayerInt = 6;
+        public const string ObstaclesLayerName = "Obstacles";
 
         public static Dictionary<TagEnum, string> Tags = new Dictionary<TagEnum, string>()
         {
